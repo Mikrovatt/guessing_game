023b87d2815777766f8bbc9d7af546328072fdf3
mikrovatt <viktorov.sergey@inbox.ru>
2022-04-26 22:23:21 +0300

    V_2 исправлены ошибки, добавлена проверка на задаваемы диапазон

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,35 @@ public class Main {
         // Приветствие
         System.out.println("Добро пожаловать в игру 'Угадайка'!");
         System.out.println("Задайте диапзон в котором я загадаю число:");
-        System.out.println("(Введите два числа через пробел и нажмите enter)");
 
         Scanner sc = new Scanner(System.in); // Инициализирую сканер для ввода данных
-        input = sc.nextLine(); // Получаю текст для диапазона
+        String[] numbers;
+        while (true) {
+            System.out.println("(Введите два числа через пробел и нажмите enter)");
+            input = sc.nextLine(); // Получаю текст для диапазона
 
-        // расчет диапазона чисел для игры
-        String[] numbers = input.split("\\s");
-        // Сделать проверку на ввод цифр и пробелов более 1
+            // расчет диапазона чисел для игры
+            input = input.trim().replaceAll(" +", " "); // убрать лишние пробелы
+            numbers = input.split("\\s");
+            if (numbers.length == 2) {
+                break;
+            } else if (numbers.length > 2) {
+                System.out.println("Не могу выбрать диапазон, слишком много чисел!");
+            } else if (numbers.length < 2 && numbers.length > 0) {
+                System.out.println("Не могу выбрать диапазон, слишком мало чисел!");
+            }
+        }
         int i[] = new int[numbers.length];
         for (int j = 0; j < numbers.length; j++) {
-            i[j] = Integer.valueOf(numbers[j]); // Перевожу текст в int, получаю диапазон для игы
-        }
-        // System.out.println(Arrays.toString(i)); // Вывод диапазона для проверки
+                i[j] = Integer.valueOf(numbers[j]); // Перевожу текст в int, получаю диапазон для игры
+            }
+            // System.out.println(Arrays.toString(i)); // Вывод диапазона для проверки
 
         int result = (int) (i[0] + Math.random() * ((i[1] - i[0]) + 1)); // Выбираю случайное число в заданом диапазоне
         // System.out.println(result); // Вывод загаданного числа дляпроверки
 
         // Начало игры, запрос числа от пользователя
-        System.out.println("Я загадал число в диапазоне от " + i[0] + " до " + i[1] + " попробуй угадай!!!");
+        System.out.println("Я загадал число в диапазоне от " + i[0] + " до " + i[1] + ", попробуй угадай!!!");
 
         int numb; // Инициализирую переменную для ввода числа пользователем
         while (true) { // сравниваю загаданное число с числом пользователя
